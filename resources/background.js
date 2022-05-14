84cbe238ad798cdcb44980375845fdaa8bfb963a
das08 <41512077+das08@users.noreply.github.com>
2022-05-14 13:31:45 +0900

    initial

---
+++ resources/background.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!***************************!*\
+  !*** ./src/background.ts ***!
+  \***************************/
+
+// For debugging
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmFja2dyb3VuZC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBYTtBQUNiIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vY29tZm9ydGFibGUtc2FrYWkvLi9zcmMvYmFja2dyb3VuZC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjtcbi8vIEZvciBkZWJ1Z2dpbmdcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
